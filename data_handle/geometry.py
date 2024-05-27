82fc788d580b45ab44bd638699df49adfb424797
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-27 12:30:45 +0200

    Update geometry.py

--- data_handle/geometry.py
+++ data_handle/geometry.py
@@ -142,7 +142,7 @@ def read_pTT(x,S1Board,CEECEH,Sector):
             pTT['Modules'].append({'module_id' : module_id, 'module_layer' : layer,'module_u' : u,'module_v' : v,'module_energy' : int(energy)})
         if CEECEH==1:
             module_id,layer,u,v,stc_idx = get_moduleCEH(x[start_module: end_module],Sector)
-            pTT['Modules'].append({'module_id' : module_id,'module_layer' : layer,'module_u' : u,'module_v' : v,'stc_idx': stc ,'module_energy' : int(energy)})
+            pTT['Modules'].append({'module_id' : module_id,'module_layer' : layer,'module_u' : u,'module_v' : v,'stc_idx': stc_idx ,'module_energy' : int(energy)})
         cursor = x[end_module+3:].find(',')
     return(pTT)
         
