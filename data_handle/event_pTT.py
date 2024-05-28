6c7ce347cfc00be476fc96f81115ae8a245211d8
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-28 14:27:47 +0200

    Update event_pTT.py

--- data_handle/event_pTT.py
+++ data_handle/event_pTT.py
@@ -89,8 +89,7 @@ class EventData():
         for pTT_idx in range(len(pTTs)):
             pTT = pTTs[pTT_idx]['pTT_id']
             pTT_xml = self.get_pTT_allocation(xml_allocation, pTT)
-            print(pTT_xml)
-            #data_pTTs[pTT_xml[0]['frame']][pTT_xml[0]['n_link']][pTT_xml[0]['channel']%2]  = pTTs[pTT_idx]['energy']
+            data_pTTs[pTT_xml[0]['frame']][pTT_xml[0]['n_link']][pTT_xml[0]['channel']%2]  = pTTs[pTT_idx]['energy']
 
         return data_pTTs
 
