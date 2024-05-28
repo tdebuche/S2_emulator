0cefcf77ade8f70a727c0233661fb660c7658759
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-28 14:33:35 +0200

    Update event_pTT.py

--- data_handle/event_pTT.py
+++ data_handle/event_pTT.py
@@ -89,9 +89,7 @@ class EventData():
         for pTT_idx in range(len(pTTs)):
             pTT = pTTs[pTT_idx]['pTT_id']
             pTT_xml = self.get_pTT_allocation(xml_allocation, pTT)
-            if pTT_xml == []:
-                print(pTT)
-            else :
+            if pTT_xml != [] :    #if pTT is allocated in the 4 links
                 data_pTTs[pTT_xml[0]['frame']][pTT_xml[0]['n_link']][pTT_xml[0]['channel']%2]  = pTTs[pTT_idx]['energy']
 
         return data_pTTs
