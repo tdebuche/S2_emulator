fb736fbea3a645494593b28d5d6623aeeda73ac3
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-28 15:31:46 +0200

    Update S1simulator.py

--- data_handle/S1simulator.py
+++ data_handle/S1simulator.py
@@ -6,19 +6,18 @@ def build_pTTsCEE(ts_energy, args, S1pTTCEE):
     else : nb_phi = 24
     Sector = args.Sector
     pTTsCEE = []
-    for S1Board in range(14):
-        for pTT_idx in range(len(S1pTTCEE)):
-            energyCEE = 0
-            pTT_id = S1pTTCEE[pTT_idx]['pTT'] 
-            ModulesCEE = S1pTTCEE[pTT_idx]['Modules'] 
-            for module_idx in range(len(ModulesCEE)):
-                module_id = ModulesCEE[module_idx]['module_id']
-                energy = ModulesCEE[module_idx]['module_energy']
-                if ts_energy[module_id] != []:
-                    energyCEE += ts_energy[module_id][0] * energy/16
-            pTTsCEE.append({'pTT_id' : pTT_id, 'energy': energyCEE})
+    for pTT_idx in range(len(S1pTTCEE)):
+        energyCEE = 0
+        pTT_id = S1pTTCEE[pTT_idx]['pTT'] 
+        ModulesCEE = S1pTTCEE[pTT_idx]['Modules'] 
+        for module_idx in range(len(ModulesCEE)):
+            module_id = ModulesCEE[module_idx]['module_id']
+            energy = ModulesCEE[module_idx]['module_energy']
+            if ts_energy[module_id] != []:
+                energyCEE += ts_energy[module_id][0] * energy/16
+                pTTsCEE.append({'pTT_id' : pTT_id, 'energy': energyCEE})
             #if energyCEE != 0:
-                #print({'pTT_id' : pTT_id, 'energy': energyCEE})
+            #print({'pTT_id' : pTT_id, 'energy': energyCEE})
 
     return(pTTsCEE)
 
