bd43a5470a8f7dc7806c4535c1fc12daabb01404
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-28 11:16:59 +0200

    Update test_packer_pTTs.py

--- data_handle/test_packer_pTTs.py
+++ data_handle/test_packer_pTTs.py
@@ -1,1 +1,36 @@
+import numpy as np
+import awkward as ak
+import uproot
+import math
+import yaml
+import os
+import argparse
 
+from S1simulator import build_pTTsCEE
+from geometry import read_txt_pTTs
+from geometry import read_xml_pTTs
+from event_pTT import provide_events
+
+parser = argparse.ArgumentParser(description='Stage-2 Emulator Parameters')
+parser.add_argument('-n',          type=int, default=1,         help='Provide the number of events')
+parser.add_argument('--particles', type=str, default='photons', help='Choose the particle sample')
+parser.add_argument('--pileup',    type=str, default='PU0',     help='Choose the pileup - PU0 or PU200')
+parser.add_argument('--plot',        action='store_true', help='Create plots')
+parser.add_argument('--col',         action='store_true', help='Create plots using column numbers')
+parser.add_argument('--phi',         action='store_true', help='Create plots using phi coordinates')
+parser.add_argument('--performance', action='store_true', help='Create performance plots: distance gen_particle/max_TC')
+parser.add_argument('--thr_seed',    action='store_true', help='Create efficiency plots post seeding')
+parser.add_argument('--cl_energy',   action='store_true', help='Create plot of gen_pt vs recontructed energy')
+parser.add_argument('--Sector',      type=int, default=0, help='Sector of S2 Board')
+parser.add_argument('--Edges',   default = 'no', help='20*24 or 20*28 bins')
+
+args = parser.parse_args()
+
+S1pTTCEE,S1pTTCEH = read_txt_pTTs(args.Edges,args.Sector)
+xml_allocation = read_xml_pTTs(args.Edges)
+
+events = provide_events(args.n, args.particles, args.pileup)
+
+events._pTT_packer(args, xml_allocation,S1pTTCEE,S1pTTCEH)
+
+print(events.pTT_packer)
