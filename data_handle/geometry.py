5c47ce3ef072d08549ecea11d7ac69584e8535b1
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-28 14:27:03 +0200

    Update geometry.py

--- data_handle/geometry.py
+++ data_handle/geometry.py
@@ -97,7 +97,7 @@ def get_pTT_id(Sector, S1Board, CEECEH, x):
         phi += x[x.find('phi')+4]
     eta = int(eta)
     phi = int(phi)
-    S1Board = (int(S1Board[4])*16 + int(S1Board[5])) & 0x3F
+    S1Board = (int(S1Board[4],16)*16 + int(S1Board[5],16)) & 0x3F
     pTT_id = hex(0x00000000 | ((Sector & 0x3) << 29) | ((1 & 0x3) << 26)  | ((6 & 0xF) << 22) | ((S1Board & 0x3F) << 16) | ((CEECEH & 0x1) << 10) | ((eta & 0x1F) << 5) | ((phi & 0x1F) << 0))
     #while len(pTT_id) <10:
     #    pTT_id = '0x'+ str(0) +pTT_id[2:]
