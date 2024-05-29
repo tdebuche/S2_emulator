a035c3569fb5d824730951b0459abd2d80371194
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-29 13:45:37 +0200

    Update plot_pTT.py

--- data_handle/plot_pTT.py
+++ data_handle/plot_pTT.py
@@ -96,7 +96,7 @@ def etaphitoXY(eta,phi,z):
 def record_plot(data_links,etaphi_links,args,title):
     energies = create_energies(data_links,etaphi_links,args)
     BinXY = create_bins(args)
-    plt.figure(figsize = (12,8))
+    plt.figure(figsize = (14,8))
     X =[]
     Y = []
     pointXY = [[],[]]
