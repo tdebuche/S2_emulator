2197edc86b63f71c2368fe95f7cabd965f78c061
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-03 10:07:10 +0200

    Update test_packer_pTTs.py

--- test_packer_pTTs.py
+++ test_packer_pTTs.py
@@ -47,7 +47,9 @@ events = provide_events(args.n, args.particles, args.pileup)
 for idx, event in enumerate(events):
   event._pTT_packer(args, xml_allocation,xml_duplication,S1pTTCEE,S1pTTCEH,S1pTTCEEdup)
   #event.provide_ts(args)
-  print(event.pTT_packer)
-  print(event.ds_ts)
+  #print(event.pTT_packer)
+  #print(event.ds_ts)
+  print(xml_plot)
   print(create_energies(event.pTT_packer,xml_plot,args))
+  
   record_plot(event.pTT_packer,xml_plot,args,'pTT_event'+str(idx))
