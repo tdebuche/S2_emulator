51958476a626af9305b0e086ed483ddfbb5df1f9
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-06 09:53:34 +0200

    Update output_EMP.py

--- data_handle/output_EMP.py
+++ data_handle/output_EMP.py
@@ -1,1 +1,17 @@
+from collections import defaultdict
+import numpy as np
+import awkward as ak
+import uproot
+import math
+import yaml
 
+f = open(output_BeforeChange + '.txt', 'r')
+EMP = f.readlines()
+f.close()
+
+print(EMP[0])
+print(EMP[1])
+print(EMP[2])
+print(EMP[3])
+print(EMP[4])
+print(EMP[5])
