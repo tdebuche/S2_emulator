02f57a92106aa5a4d1fc88ce89ca07896e4b8366
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-08 13:48:15 +0200

    Update event_pTT.py

--- data_handle/event_pTT.py
+++ data_handle/event_pTT.py
@@ -109,25 +109,23 @@ class EventData():
         ts = defaultdict(list)
         Sector = args.Sector
         for module_idx in range(len(self.ds_si.good_tc_layer)):
-            if not self.getuvsector(self.ds_si.good_tc_layer[module_idx][0],
-                                            self.ds_si.good_tc_waferu[module_idx][0],
-                                            self.ds_si.good_tc_waferv[module_idx][0]):
+            if not self.getuvsector(self.ds_si.good_tc_layer[module_idx][0],self.ds_si.good_tc_waferu[module_idx][0],self.ds_si.good_tc_waferv[module_idx][0]):
                 print(self.ds_si.good_tc_layer[module_idx][0],
                                             self.ds_si.good_tc_waferu[module_idx][0],
                                             self.ds_si.good_tc_waferv[module_idx][0])
-            if self.ds_si.good_tc_waferu[module_idx][0] != -999:
+            else :
                 u,v,sector = self.getuvsector(self.ds_si.good_tc_layer[module_idx][0],
                                             self.ds_si.good_tc_waferu[module_idx][0],
                                             self.ds_si.good_tc_waferv[module_idx][0])
-            if u != -999:
-                module = self.get_module_id(sector,
-                                            self.ds_si.good_tc_layer[module_idx][0],
-                                            u,
-                                            v)
-                if self.ds_si.good_tc_layer[module_idx][0] < 48:
-                    if ts[module] == []:
-                        ts[module].append(0)
-                    ts[module][0] += self.ds_si.good_tc_pt[module_idx][0]
+                if u != -999:
+                    module = self.get_module_id(sector,
+                                                self.ds_si.good_tc_layer[module_idx][0],
+                                                u,
+                                                v)
+                    if self.ds_si.good_tc_layer[module_idx][0] < 48:
+                        if ts[module] == []:
+                            ts[module].append(0)
+                        ts[module][0] += self.ds_si.good_tc_pt[module_idx][0]
         self.ds_ts = ts
             
                 
