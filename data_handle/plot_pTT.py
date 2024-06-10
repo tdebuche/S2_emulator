9c7d322f884b9272ccefecd8ec216899281976a2
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-10 14:20:05 +0200

    Update plot_pTT.py

--- data_handle/plot_pTT.py
+++ data_handle/plot_pTT.py
@@ -179,6 +179,7 @@ def createplot(args,event,energies,BinXY,title):
                 weights.append(0)
             pointXY[0].append(np.sum(np.array(BinXY[eta][phi][0][0:4]))/4)
             pointXY[1].append(np.sum(np.array(BinXY[eta][phi][1][0:4]))/4)
+    if weightmax == 0: weightmax = 1
     sc = plt.scatter(pointXY[0],pointXY[1],c=weights, vmin=0)
     plt.colorbar(sc)
     res = 0 
