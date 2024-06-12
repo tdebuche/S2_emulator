2e180b9ebe0c4d4eac37c1902512ce707ea82c06
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-12 10:40:03 +0200

    Update event_pTT.py

--- data_handle/event_pTT.py
+++ data_handle/event_pTT.py
@@ -78,10 +78,10 @@ class EventData():
             xml_alloc = self.get_TC_allocation(xml[0], module)
             if xml_alloc: 
                 n_TCs = xml_alloc[-1]['index']
-                selected_TCs.ds_si.good_tc_phi[module_idx]  = selected_TCs.ds_si.good_tc_phi[module_idx][:n_TCs+1]
-                selected_TCs.ds_si.good_tc_r_over_z[module_idx]  = selected_TCs.good_tc_r_over_z[module_idx][:n_TCs+1]           
-                selected_TCs.ds_si.good_tc_pt[module_idx]  = selected_TCs.ds_si.good_tc_pt[module_idx][:n_TCs+1]
-                selected_TCs.ds_si.good_tc_layer[module_idx]  = selected_TCs.ds_si.good_tc_layer[module_idx][:n_TCs+1]
+                selected_TCs.good_tc_phi[module_idx]  = selected_TCs.good_tc_phi[module_idx][:n_TCs+1]
+                selected_TCs.good_tc_r_over_z[module_idx]  = selected_TCs.good_tc_r_over_z[module_idx][:n_TCs+1]           
+                selected_TCs.good_tc_pt[module_idx]  = selected_TCs.good_tc_pt[module_idx][:n_TCs+1]
+                selected_TCs.good_tc_layer[module_idx]  = selected_TCs.good_tc_layer[module_idx][:n_TCs+1]
                 nb_selected_TCs[module].append[n_TCs]  
 
         TCs = self.ds_si
