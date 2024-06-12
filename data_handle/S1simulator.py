424349fddcfe133d2023170897e2a0e58ab438a9
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-12 12:28:50 +0200

    Update S1simulator.py

--- data_handle/S1simulator.py
+++ data_handle/S1simulator.py
@@ -1,5 +1,8 @@
 import numpy as np
 import awkward as ak
+from data_handle.tools import getuvsector
+
+
 
 def build_pTTsCEE(ts_energy, args, S1pTTCEE):
     if args.Edges == 'yes': nb_phi = 28
@@ -40,5 +43,30 @@ def build_pTTsCEH(stc_energy,args,S1pTTCEH):
     return(pTTsCEH)
 
 def add_TCs(pTTs,TCs,nb_selected_TCs, Sector,CEECEH):
+    energytoadd= defaultdict(list)
+    for module_idx in range(len(TCs.good_tc_layer)):
+        if ((TCs.good_tc_layer[module_idx][0] < 27) and (CEECEH == 'CEE' )) or ((TCs.good_tc_layer[module_idx][0] >= 27) and (CEECEH == 'CEH' )):
+            u,v,sector = getuvsector(TCs.good_tc_layer[module_idx][0],
+                                        TCs.ds_si.good_tc_waferu[module_idx][0],
+                                        TCs.ds_si.good_tc_waferv[module_idx][0])
+            module = get_module_id(Sector, TCs.good_tc_layer[module_idx][0], u, v)
+            if sector == Sector:
+                for idx in range(len(nb_selected_TCs[module])
+                    eta,phi = getetaphi(TCs.ds_si.good_tc_phi[module_idx][idx],TCs.ds_si.good_tc_r_over_z[module_idx][idx])
+                    S1_Board = S1_Board(TCs.good_tc_layer[module_idx][idx])
+                    if CEECEH == 'CEE': a = 0
+                    if CEECEH == 'CEH': a = 1
+                    pTT = get_pTT_id(Sector, S1Board, a, eta,phi)
+                energytoadd[pTT].append(TCs.ds_si.good_tc_pt[module_idx][idx])
     return pTTs
-                    
+
+
+def get_pTT_id(Sector, S1Board, CEECEH, eta,phi):
+    S1Board = (int(S1Board[4],16)*16 + int(S1Board[5],16)) & 0x3F
+    pTT_id = hex(0x00000000 | ((Sector & 0x3) << 29) | ((1 & 0x3) << 26)  | ((6 & 0xF) << 22) | ((S1Board & 0x3F) << 16) | ((CEECEH & 0x1) << 10) | ((eta & 0x1F) << 5) | ((phi & 0x1F) << 0))
+    #while len(pTT_id) <10:
+    #    pTT_id = '0x'+ str(0) +pTT_id[2:]
+    return pTT_id
+
+def get_module_id(Sector, plane, u, v):
+    return hex(0x00000000 |  ((Sector & 0x3) << 29) | ((0 & 0x3) << 26)  | ((0 & 0xF) << 22) |  ((plane & 0x3F) << 16) | ((u & 0xF) << 12) | ((v & 0xF) << 8))
