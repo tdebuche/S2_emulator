0d68942494ef3fad354bfbc475444267a4938cdc
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-12 11:49:51 +0200

    Update event_pTT.py

--- data_handle/event_pTT.py
+++ data_handle/event_pTT.py
@@ -75,8 +75,8 @@ class EventData():
                                         self.ds_si.good_tc_waferu[module_idx][0],
                                         self.ds_si.good_tc_waferv[module_idx][0])
             module_alloc = self.get_module_id(3,self.ds_si.good_tc_layer[module_idx][0],u,v)
-            xml_alloc = self.get_TC_allocation(xml[0], module_alloc)
             module = self.get_module_id(sector,self.ds_si.good_tc_layer[module_idx][0],u,v)
+            xml_alloc = self.get_TC_allocation(xml[0], module_alloc)
             if xml_alloc: 
                 n_TCs = xml_alloc[-1]['index']
                 nb_selected_TCs[module].append(n_TCs)  
@@ -94,8 +94,12 @@ class EventData():
                     if ts[module] == []:
                         ts[module].append(0)
                     #for idx in range(len(self.ds_si.good_tc_layer[module_idx])):
-                    for idx in range(nb_selected_TCs[module][0],len(self.ds_si.good_tc_layer[module_idx])):
-                        ts[module][0] += self.ds_si.good_tc_pt[module_idx][idx]
+                    if nb_selected_TCs[module]:
+                        for idx in range(nb_selected_TCs[module][0],len(self.ds_si.good_tc_layer[module_idx])):
+                            ts[module][0] += self.ds_si.good_tc_pt[module_idx][idx]
+                    else : 
+                        print( u,v,sector)
+        
         self.ds_ts = ts
         return(nb_selected_TCs)
         
